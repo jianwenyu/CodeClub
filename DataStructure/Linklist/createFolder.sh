05a639b89bbcc38b3387aba52b3fbb5cdfda87fa
Jerry Yu <jerryyu1202@gmail.com>
2019-11-23 11:14:57 -0500

    [J.Y] Improve make file

---
+++ DataStructure/Linklist/createFolder.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+echo "Checking file structure correction.."
+
+if [ ! -d "./obj" ]
+then
+echo "obj folder not exist, create obj file"
+mkdir obj
+else
+echo "obj exist, you are good to go"
+fi
+
+if [ ! -d "./debug" ]
+then
+echo "debug folder not exist, creating debug file"
+mkdir debug
+else
+echo "debug exist, you are good to go"
+fi
+
+if [ ! -d "./release" ]
+then
+echo "release folder not exist, creating release file"
+mkdir release
+else
+echo "release exist, you are good to go"
+fi
